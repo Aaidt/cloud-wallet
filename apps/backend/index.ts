5a60c33681f4ed2862609181707ff758afaa7d11
Aadit <aadits2004@gmail.com>
2025-11-26 15:06:37 +0530

    init db

---
+++ apps/backend/index.ts
@@ -0,0 +1,13 @@
+import express from "express"
+import cors from "cors"
+
+const app = express();
+app.use(cors())
+
+app.get("/", (req, res) => {
+   res.send("Hello from Express!");
+});
+
+app.listen(3000, () => {
+   console.log("Server is running on port 3000");
+});
